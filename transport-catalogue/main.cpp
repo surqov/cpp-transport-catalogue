73f8e970926b302da9ff617f1d3d8757472faa0e
Gleb Surkov <writeln7@gmail.com>
2023-02-21 22:02:06 +0100

    done

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,17 +1,20 @@
 #include "input_reader.h"
 #include "transport_catalogue.h"
 #include "stat_reader.h"
+#include "log_duration.h"
 
 #include <fstream>
 
 using namespace std::literals;
 
 int main() {
-    //std::fstream input("assert_files/test_from_yandex.txt");
-    std::fstream input("assert_files/tsA_case1_input.txt");
+    {//std::fstream input("assert_files/test_from_yandex.txt");
+        LOG_DURATION("File stream");
+        std::fstream input("assert_files/tsA_case2_input.txt");
 
-    reader<std::fstream> queries(input);
-    bus_catalogue catalog(queries.GetQueries());
-    stat<std::fstream> output(input, catalog);
+        reader<std::fstream> queries(input);
+        bus_catalogue catalog(queries.GetQueries());
+        stat<std::fstream> output(input, catalog);
+    }
     return 0;
 }
